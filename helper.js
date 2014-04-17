48c357e8171b3ea166b571b29613a72ec0ffb7ad
Ryan Hansberry <rhansby@gmail.com>
2014-04-16 18:20:34 -0700

    Added comment to helper.js file for clarification

--- helper.js
+++ helper.js
@@ -1,3 +1,5 @@
+// Commonly needed functions (e.g. Schema validation) are recommended to be placed here.
+
 // Ensure that important fields are not empty:
 module.exports.isNotEmpty = function(str) {
     return str && str.length;
