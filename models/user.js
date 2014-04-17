32b9b09dade6c3401f0b85b050c991be8a587339
Ryan Hansberry <rhansby@gmail.com>
2014-04-16 20:03:54 -0700

    Separated model and routing logic.

--- models/user.js
+++ models/user.js
@@ -1,13 +1,18 @@
 var mongoose = require('mongoose'),
     helper = require('../helper');
 
+// Schema:
 var userSchema = mongoose.Schema({
     name: {type: String, required: true},
-    email: {type: String, required: true},
+    email: {type: String, index: {
+        unique: true,
+        required: true
+    }},
     password: {type: String, required: true}
 }),
     User = mongoose.model('User', userSchema);
 
+// Validations:
 User.schema.path('email').validate(function(value) {
     // One or more chars + "@" + 1 or more chars + "." + 1 or more chars + [nothing else]:
     return /.+@.+\..+$/.test(value)
@@ -15,5 +20,32 @@ User.schema.path('email').validate(function(value) {
 
 User.schema.path('name').validate(helper.isNotEmpty, 'name cannot be empty');
 User.schema.path('password').validate(helper.isNotEmpty, 'password cannot be empty');
+
+// Methods:
+User.register = function(data, callback, errback) {
+    var new_user = new User({
+        name: data.name,
+        email: data.email,
+        password: data.password
+    });
+
+    new_user.save(function(err) {
+        if (err) {
+            return errback();
+        }
+
+        return callback(new_user);
+    });
+};
+
+User.login = function(user_id, callback, errback) {
+    User.findById(user_id, function(err, user) {
+        if (! user || err) {
+            return errback();
+        }
+
+        return callback(user);
+    });
+}
 
 module.exports = User;
--- routes/user.js
+++ routes/user.js
@@ -2,37 +2,31 @@ var restify = require('restify'),
     helper = require('../helper'),
     User = require('../models/user.js');
 
-module.exports.login = function(req, res, next) {
-    var user_id = req.authorization.basic.username;
-
-    User.findById(user_id, function(err, user) {
-        if (! user || err) {
+module.exports.login = function(req, res, next) {`
+    User.login(
+        req.authorization.basic.username,
+        function(user) {
+            res.send({
+                name: user.name,
+                email: user.email
+            });
+            return next();
+        },
+        function() {
             return next(new restify.InternalError('Something went wrong.'));
         }
-
-        res.send({
-            name: user.name,
-            email: user.email
-        });
-        return next();
-    });
+    );
 };
 
 module.exports.register = function(req, res, next) {
-    var data = {
-        name: req.params.name,
-        email: req.params.email,
-        password: req.params.password
-    };
-
-    var new_user = new User(data);
-
-    new_user.save(function(err) {
-        if (err) {
+    User.register(
+        req.params,
+        function onSuccess(user) {
+            res.send(201, {id: user._id});
+            return next();
+        },
+        function onError() {
             return next(new restify.InvalidArgumentError('User creation failed.'));
         }
-
-        res.send(201, {id: new_user._id});
-        return next();
-    });
+    );
 };
